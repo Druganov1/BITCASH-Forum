1ccfd0359d142198d3291799e398274f3813ba70
Druganov <nawidkg@outlook.com>
2023-11-01 11:44:07 +0100

    Update database dump, outdated

--- database/dbforum.sql
+++ database/dbforum.sql
@@ -1,249 +1,109 @@
--- phpMyAdmin SQL Dump
--- version 4.2.11
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Mar 23, 2015 at 07:53 PM
--- Server version: 5.6.21
--- PHP Version: 5.6.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server version:               8.0.30 - MySQL Community Server - GPL
+-- Server OS:                    Win64
+-- HeidiSQL Version:             12.1.0.6537
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
---
--- Database: `dbforum`
---
 
--- --------------------------------------------------------
+-- Dumping database structure for dbforum
+CREATE DATABASE IF NOT EXISTS `dbforum` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `dbforum`;
 
---
--- Table structure for table `category`
---
-
+-- Dumping structure for table dbforum.category
 CREATE TABLE IF NOT EXISTS `category` (
-`cat_id` int(12) NOT NULL,
-  `category` varchar(200) DEFAULT NULL
+  `cat_id` int NOT NULL AUTO_INCREMENT,
+  `category` varchar(200) DEFAULT NULL,
+  PRIMARY KEY (`cat_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `category`
---
+-- Data exporting was unselected.
 
-INSERT INTO `category` (`cat_id`, `category`) VALUES
-(1, 'programming'),
-(6, 'networking'),
-(7, 'java');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblaccount`
---
-
+-- Dumping structure for table dbforum.tblaccount
 CREATE TABLE IF NOT EXISTS `tblaccount` (
-`accnt_Id` int(11) NOT NULL,
+  `accnt_Id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
-  `user_Id` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
+  `user_Id` int DEFAULT NULL,
+  PRIMARY KEY (`accnt_Id`),
+  KEY `user_Id` (`user_Id`),
+  CONSTRAINT `tblaccount_ibfk_1` FOREIGN KEY (`user_Id`) REFERENCES `tbluser` (`user_Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tblaccount`
---
+-- Data exporting was unselected.
 
-INSERT INTO `tblaccount` (`accnt_Id`, `username`, `password`, `user_Id`) VALUES
-(8, 'mcdo', '8542c29a0773a42de1f302903b9e2d74', 13),
-(9, 'mar', '5fa9db2e335ef69a4eeb9fe7974d61f4', 14);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbladmin`
---
-
+-- Dumping structure for table dbforum.tbladmin
 CREATE TABLE IF NOT EXISTS `tbladmin` (
-`Id` int(11) NOT NULL,
+  `Id` int NOT NULL AUTO_INCREMENT,
   `uname` varchar(255) DEFAULT NULL,
-  `pwd` varchar(255) DEFAULT NULL
+  `pwd` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`Id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tbladmin`
---
+-- Data exporting was unselected.
 
-INSERT INTO `tbladmin` (`Id`, `uname`, `pwd`) VALUES
-(1, 'admin', 'admin'),
-(2, 'admin', 'admin');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblcomment`
---
-
+-- Dumping structure for table dbforum.tblcomment
 CREATE TABLE IF NOT EXISTS `tblcomment` (
-`comment_Id` int(11) NOT NULL,
-  `comment` varchar(255) DEFAULT NULL,
-  `post_Id` int(11) DEFAULT NULL,
+  `comment_Id` int NOT NULL AUTO_INCREMENT,
+  `comment` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `post_Id` int DEFAULT NULL,
   `datetime` datetime DEFAULT NULL,
-  `user_Id` int(12) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+  `user_Id` int DEFAULT NULL,
+  PRIMARY KEY (`comment_Id`),
+  KEY `user_Id` (`user_Id`),
+  KEY `post_Id` (`post_Id`),
+  KEY `user_Id_2` (`user_Id`),
+  CONSTRAINT `tblcomment_ibfk_1` FOREIGN KEY (`user_Id`) REFERENCES `tbluser` (`user_Id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `tblcomment_ibfk_2` FOREIGN KEY (`post_Id`) REFERENCES `tblpost` (`post_Id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tblcomment`
---
+-- Data exporting was unselected.
 
-INSERT INTO `tblcomment` (`comment_Id`, `comment`, `post_Id`, `datetime`, `user_Id`) VALUES
-(4, 'my', 11, '2015-03-24 02:52:00', 13),
-(5, 'mar', 11, '2015-03-24 02:52:27', 14);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tblpost`
---
-
+-- Dumping structure for table dbforum.tblpost
 CREATE TABLE IF NOT EXISTS `tblpost` (
-`post_Id` int(11) NOT NULL,
+  `post_Id` int NOT NULL AUTO_INCREMENT,
   `title` varchar(255) DEFAULT NULL,
   `content` text,
   `datetime` datetime DEFAULT NULL,
-  `cat_id` int(12) DEFAULT NULL,
-  `user_Id` int(12) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
+  `cat_id` int DEFAULT NULL,
+  `user_Id` int DEFAULT NULL,
+  PRIMARY KEY (`post_Id`),
+  KEY `cat_id` (`cat_id`),
+  KEY `user_Id` (`user_Id`),
+  CONSTRAINT `tblpost_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `category` (`cat_id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tblpost`
---
+-- Data exporting was unselected.
 
-INSERT INTO `tblpost` (`post_Id`, `title`, `content`, `datetime`, `cat_id`, `user_Id`) VALUES
-(11, 'sample', 'sample admin\r\n                        ', '2015-03-24 02:46:55', 1, 0),
-(13, 'sample', 'user                        ', '2015-03-24 02:49:20', 1, 13);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tbluser`
---
-
+-- Dumping structure for table dbforum.tbluser
 CREATE TABLE IF NOT EXISTS `tbluser` (
-`user_Id` int(11) NOT NULL,
+  `user_Id` int NOT NULL AUTO_INCREMENT,
   `fname` varchar(255) DEFAULT NULL,
   `lname` varchar(255) DEFAULT NULL,
-  `gender` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=latin1;
+  `gender` varchar(255) DEFAULT NULL,
+  `street` varchar(255) DEFAULT NULL,
+  `houseno` varchar(255) DEFAULT NULL,
+  `postalno` varchar(255) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
+  `phonenumber` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `preposition` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`user_Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tbluser`
---
+-- Data exporting was unselected.
 
-INSERT INTO `tbluser` (`user_Id`, `fname`, `lname`, `gender`) VALUES
-(13, 'mcdo', 'mcdo', 'Male'),
-(14, 'mar', 'mar', 'Female');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `category`
---
-ALTER TABLE `category`
- ADD PRIMARY KEY (`cat_id`);
-
---
--- Indexes for table `tblaccount`
---
-ALTER TABLE `tblaccount`
- ADD PRIMARY KEY (`accnt_Id`), ADD KEY `user_Id` (`user_Id`);
-
---
--- Indexes for table `tbladmin`
---
-ALTER TABLE `tbladmin`
- ADD PRIMARY KEY (`Id`);
-
---
--- Indexes for table `tblcomment`
---
-ALTER TABLE `tblcomment`
- ADD PRIMARY KEY (`comment_Id`), ADD KEY `user_Id` (`user_Id`), ADD KEY `post_Id` (`post_Id`), ADD KEY `user_Id_2` (`user_Id`);
-
---
--- Indexes for table `tblpost`
---
-ALTER TABLE `tblpost`
- ADD PRIMARY KEY (`post_Id`), ADD KEY `cat_id` (`cat_id`), ADD KEY `user_Id` (`user_Id`);
-
---
--- Indexes for table `tbluser`
---
-ALTER TABLE `tbluser`
- ADD PRIMARY KEY (`user_Id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `category`
---
-ALTER TABLE `category`
-MODIFY `cat_id` int(12) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
---
--- AUTO_INCREMENT for table `tblaccount`
---
-ALTER TABLE `tblaccount`
-MODIFY `accnt_Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=10;
---
--- AUTO_INCREMENT for table `tbladmin`
---
-ALTER TABLE `tbladmin`
-MODIFY `Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=3;
---
--- AUTO_INCREMENT for table `tblcomment`
---
-ALTER TABLE `tblcomment`
-MODIFY `comment_Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `tblpost`
---
-ALTER TABLE `tblpost`
-MODIFY `post_Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=14;
---
--- AUTO_INCREMENT for table `tbluser`
---
-ALTER TABLE `tbluser`
-MODIFY `user_Id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=15;
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `tblaccount`
---
-ALTER TABLE `tblaccount`
-ADD CONSTRAINT `tblaccount_ibfk_1` FOREIGN KEY (`user_Id`) REFERENCES `tbluser` (`user_Id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `tblcomment`
---
-ALTER TABLE `tblcomment`
-ADD CONSTRAINT `tblcomment_ibfk_1` FOREIGN KEY (`user_Id`) REFERENCES `tbluser` (`user_Id`) ON DELETE CASCADE ON UPDATE CASCADE,
-ADD CONSTRAINT `tblcomment_ibfk_2` FOREIGN KEY (`post_Id`) REFERENCES `tblpost` (`post_Id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
---
--- Constraints for table `tblpost`
---
-ALTER TABLE `tblpost`
-ADD CONSTRAINT `tblpost_ibfk_1` FOREIGN KEY (`cat_id`) REFERENCES `category` (`cat_id`) ON DELETE CASCADE ON UPDATE CASCADE;
-
+/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
